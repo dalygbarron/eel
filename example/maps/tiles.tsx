8ecacee1b7f9abaa6c0420bee405a7b2d867794b
Daly Graham Barron <dalygbarron@gmail.com>
2019-09-21 16:30:59 +0100

    adding pics

---
+++ example/maps/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles" tilewidth="64" tileheight="96" tilecount="96" columns="32">
+ <image source="../images/tiles.png" width="2048" height="288"/>
+</tileset>
